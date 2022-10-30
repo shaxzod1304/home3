2660c4bf1da3245273e02a8d6f006ae591765a7e
Shaxzod <tursunmuratoshaxzod@gmail.com>
2022-10-30 18:55:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let  animals = ["tiger", "lion", "hippopotamus", "girafe"]
+
+//1
+alert( animals.length )
+
+//2
+alert( animals[0]);
+alert( animals[1]);
+alert( animals[2]);
+alert( animals[3]);
+
+//3
+let arr = [ 'Яблоко', { name: 'Джон' }, true, function() { alert('привет'); } ];
+alert( arr[1].name );
+arr[3](); 
